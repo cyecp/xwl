c5727a92d3497d321d06abdec6cec5cdec7b663a
Adam Petrone <adampetrone83@gmail.com>
2012-11-07 22:52:44 -0500

    Updated pbuild script with outputs.

--- xwl.py
+++ xwl.py
@@ -1,5 +1,5 @@
 class xwlBuilder(Builder): 
-	def config(self, platform, builder, defaults):
+	def config(self, platform, builder):
 		builder.includes = ['include']
 		builder.libs = ['xwl']
 		builder.projects = ['xwl']
@@ -18,3 +18,17 @@ class xwlBuilder(Builder):
 	def postclean(self, driver, args):
 		d = Delete(path=args['libpath'], directory=True)
 		d.run()
+
+	def outputs(self, platform, builder, driver, args):
+		outputs = []
+
+		#builder.addOutput( name="xwl", type="static" )
+
+		if platform is LINUX:
+			outputs.append( "libxwl.a" )
+		elif platform is MACOSX:
+			outputs.append( "libxwl.a" )
+		elif platform is WINDOWS:
+			outputs.append( "xwl.lib" )
+
+		return outputs
