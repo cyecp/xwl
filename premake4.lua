710bc3fc95ad2a9c71429d089314df704634a86c
Adam Petrone <adampetrone83@gmail.com>
2013-03-24 01:22:39 +0000

    Adjusted scripts to work better with pegasus scripts. Added separate pegasus script to build on the RaspberryPi

--- premake4.lua
+++ premake4.lua
@@ -5,7 +5,7 @@ newoption {
 }
 
 newoption {
-	trigger = "with-egl",
+	trigger = "with_egl",
 	value = nil,
 	description = "Enable EGL support, adds -DEGL=1, plus platform files."
 }
@@ -14,7 +14,7 @@ if _OPTIONS["rpi"] then
 	print( "Compiling for the RaspberryPi..." )
 end
 
-if _OPTIONS["with-egl"] then
+if _OPTIONS["with_egl"] then
 	print( "Compiling with EGL..." )
 end
 
@@ -122,7 +122,7 @@ project "xwl"
 
 		if _OPTIONS["rpi"] ~= nil then
 			setup_raspberry_pi()
-		elseif _OPTIONS["with-egl"] ~= nil then
+		elseif _OPTIONS["with_egl"] ~= nil then
 			setup_egl()
 		end
 
@@ -205,7 +205,7 @@ project "sample"
 		
 		if _OPTIONS["rpi"] ~= nil then
 			setup_raspberry_pi()
-		elseif _OPTIONS["with-egl"] ~= nil then
+		elseif _OPTIONS["with_egl"] ~= nil then
 			setup_egl()
 		end
 
---
+++ xwl_raspberrypi.py
@@ -0,0 +1,40 @@
+class xwlRasberryPiBuilder(Builder):
+	def setup(*args, **kwargs):
+		builder = kwargs.get( "builder", None )
+		p = Project( name="xwl" )
+		builder.addProject( p )
+
+	def config(*args, **kwargs):
+		host_platform = kwargs.get( "host_platform", None )
+		target_platform = kwargs.get( "target_platform", host_platform )
+		driver = kwargs.get( "driver", None )
+		builder = kwargs.get( "builder", None )
+		project = kwargs.get( "project", None )
+		params = kwargs.get( "args", None )
+
+		libdir = "lib/%s/%s" % (params['architecture'], params['configuration'])
+
+		if target_platform is LINUX:
+			items = ['lib']
+			items.append( params['build_architecture'] )
+			items.append( params['configuration'] )
+			libdir = '/'.join( items )
+
+		builder.includes = ['include']
+		builder.libs = [ project.name ]
+		builder.setOutput( path=libdir, name=project.name, type=Builder.DynamicLibrary )
+
+		driver.config = (params['configuration'].lower() + Premake4.archmap[ params['platform'] ][ params['build_architecture'] ])
+
+
+	def generate(self, *args, **kwargs):
+		builder = kwargs.get( "builder", None )
+		premake = Premake4( action=builder.premake_action )
+		premake.with_egl = True
+		premake.rpi = True
+		premake.run()
+
+	def postclean(*args, **kwargs):
+		params = kwargs.get( "args", None )
+		d = Delete(path=os.path.join(params['libpath'], os.path.pardir), directory=True)
+		d.run()
