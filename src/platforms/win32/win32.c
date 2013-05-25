57903f07ad3436e5ec6679f66e6e27026d44c768
Adam Petrone <adampetrone83@gmail.com>
2013-05-25 17:49:00 -0400

    Added missing file

---
+++ src/platforms/win32/win32.c
@@ -0,0 +1,7 @@
+#include <xwl/xwl.h>
+#include <xwl/xwl_internal.h>
+#include <xwl/platforms/win32/win32.h>
+
+
+pfnChoosePixelFormatARB _win32ChoosePixelFormatARB = 0;
+pfnCreateContextAttribsARB _win32CreateContextAttribsARB = 0;
